42d5092e6b803e819b5955bbae5e4605989932be
Faizal <Faizal@pacmann.ai>
2022-02-13 15:54:26 +0800

    menupdate file main.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-a=int(input("Maskkan angka 1: "))
-b=int(input("Maskkan angka 2: "))
 
-hasil = a+b
-print(hasil)
+from class_calc import Calculator
+
+# program untuk menjalankan calculator
+while True:
+        
+        user_input = input("Tuliskan nilai yang ingin dihitung: ")
+        user_input = user_input.strip().split(" ")
+                
+        user_number1 = int(user_input[0])
+        user_number2 = int(user_input[2])
+        decision = user_input[1]
+                                
+        calc = Calculator(user_number1, user_number2)
+        result = calc.decision(decision)
+        print(result)
+                                                
+        lanjut = input("Anda ingin melanjutkan perhitungan?: ")
+                                                        
+        if lanjut.lower()[0] == 'y':
+             continue
+        else: break
